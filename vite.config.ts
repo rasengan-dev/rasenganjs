a10d39bf7989caba6d52acb079aba12ef331b3a7
dilane3 <komboudilane125@gmail.com>
2023-12-30 00:33:07 +0100

    Removing all things that relates to Image component

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
 
@@ -15,19 +15,13 @@ export default defineConfig({
   },
 
   ssr: {
+    // Ignore CSS files
     noExternal: [/\.css$/],
   },
 
-  resolve: {
-    extensions: [
-      ".mjs",
-      ".js",
-      ".ts",
-      ".jsx",
-      ".tsx",
-      ".json",
-      ".css",
-      ".scss",
-    ],
-  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  }
 });
