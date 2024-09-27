a2e1de9dd45a9387474244ef38bcc0c0ea9ba7ea
dilane3 <komboudilane125@gmail.com>
2024-09-28 00:43:06 +0100

    feat: Adding theme package

---
+++ packages/theme/release/README.md
@@ -0,0 +1,1 @@
+This folder is used in development mode to store all packages released locally in order to be tested.
---
+++ packages/theme/src/index.ts
@@ -0,0 +1,6 @@
+import useTheme from "./hooks/useTheme.js";
+import { type ThemesType, Themes } from "./types/index.js";
+import Provider from "./components/Provider.js";
+
+export { useTheme, Themes, ThemesType };
+export default Provider;
