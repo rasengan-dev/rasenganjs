ee3ef40c815cf99da868778c8fe6bd521861db5f
dilane3 <komboudilane125@gmail.com>
2024-10-14 19:31:18 +0100

    refactor: Remove the signals package from rasenganjs

--- packages/signals/release/README.md
+++
@@ -1,1 +0,0 @@
-This folder is used in development mode to store all packages released locally in order to be tested.
--- packages/signals/src/hooks/useAllSignals.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import Context from "../contexts/index.js";
-
-export default function useAllSignals() {
-	// Global state that manage all signals
-	const { signals } = useContext(Context);
-
-	return signals;
-}
