cb714773d932f9b151d3e915bff9a674153580df
dilane3 <komboudilane125@gmail.com>
2025-02-14 20:40:35 +0100

    feat: Adding some workflow and scripts

---
+++ scripts/release.ts
@@ -0,0 +1,49 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { release } from '@vitejs/release-scripts';
+import colors from 'picocolors';
+
+release({
+  repo: 'rasenganjs',
+  packages: [
+    'rasengan',
+    'rasengan-image',
+    'rasengan-mdx',
+    'rasengan-serve',
+    'rasengan-theme',
+    'rasengan-vercel',
+    'create-rasengan',
+  ],
+  toTag: (pkg, version) => {
+    if (pkg === 'create-rasengan') {
+      return `${pkg}@${version}`;
+    }
+
+    if (pkg === 'rasengan') {
+      return `v${version}`;
+    }
+
+    return `@rasenganjs/${pkg.split('-')[1]}@${version}`;
+  },
+  logChangelog: async (pkgName) => {
+    const changelog = readFileSync(`packages/${pkgName}/CHANGELOG.md`, 'utf-8');
+    if (!changelog.includes('## Unreleased')) {
+      throw new Error("Can't find '## Unreleased' section in CHANGELOG.md");
+    }
+    const index = changelog.indexOf('## Unreleased') + 13;
+    console.log(
+      colors.dim(changelog.slice(index, changelog.indexOf('## ', index)).trim())
+    );
+  },
+  generateChangelog: async (pkgName, version) => {
+    const changelog = readFileSync(`packages/${pkgName}/CHANGELOG.md`, 'utf-8');
+    console.log(colors.cyan('\nUpdating CHANGELOG.md...'));
+    const date = new Date().toISOString().slice(0, 10);
+    writeFileSync(
+      `packages/${pkgName}/CHANGELOG.md`,
+      changelog.replace(
+        '## Unreleased',
+        `## Unreleased\n\n## ${version} (${date})`
+      )
+    );
+  },
+});
