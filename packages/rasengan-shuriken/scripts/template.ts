8ae28363ab7b141aa05b9461fda030897346435a
dilane3 <komboudilane125@gmail.com>
2025-03-15 12:19:21 +0100

    feat: update the logic of cloning template

--- packages/rasengan-shuriken/scripts/template.ts
+++ packages/rasengan-shuriken/scripts/template.ts
@@ -60,16 +60,15 @@ export default async function createProjectFromTemplate(
     console.log('');
 
     // Starting the spinner for creating the project
-    const createSpinner = spinner('Creating project...');
+    const createSpinner = spinner(
+      `Cloning template into '${chalk.bold.blue(projectPath.split('/').pop())}'`
+    );
 
     createSpinner.start();
 
-    await sleep(1000);
-    createSpinner.text = `Cloning template into ${projectPath.split('/').pop()}...`;
-
     try {
       // Clone the template repository
-      await git.clone(REPOSITORY, '.tmp');
+      await git.clone(REPOSITORY, '.tmp', ['--no-checkout', '--depth', '1']);
     } catch (error) {
       console.error(error);
       createSpinner.fail(
@@ -84,12 +83,30 @@ export default async function createProjectFromTemplate(
       return;
     }
 
+    // cd into the temporary folder
+    await git.cwd(tmpFolder);
+
+    // Activate the git sparse-checkout feature
+    await git.raw(['sparse-checkout', 'init', '--cone']);
+
+    // Add the meta.json file to the sparse-checkout
+    await git.raw(['sparse-checkout', 'set', 'meta.json']);
+
+    // Extract the meta.json
+    await git.raw(['checkout', 'HEAD', '--', 'meta.json']);
+
     // Read the meta.json file from the temporary folder
     const metaFilePath = path.join(tmpFolder, 'meta.json');
     const metaData = await fs.readFile(metaFilePath, 'utf-8');
     const metaDataJson = JSON.parse(metaData);
 
     const templatePath = metaDataJson[projectKey].path;
+
+    // Add the template path to the sparse-checkout
+    await git.raw(['sparse-checkout', 'set', templatePath]);
+
+    // Extract the template files
+    await git.raw(['checkout', 'HEAD', '--', templatePath]);
 
     const srcFolder = path.posix.join(tmpFolder, `/${templatePath}`);
 
@@ -109,8 +126,12 @@ export default async function createProjectFromTemplate(
       return;
     }
 
-    await sleep(2000);
-    createSpinner.text = 'Copying the template files...';
+    createSpinner.stopAndPersist({
+      symbol: chalk.green('✔'),
+      text: `Project ${chalk.bold.blue(projectKey)} cloned successfully!`,
+    });
+
+    createSpinner.start('Copying the template files');
 
     // Copying the template files to the project directory
     ncp(srcFolder, projectPath, async (err) => {
@@ -127,7 +148,13 @@ export default async function createProjectFromTemplate(
       }
 
       await sleep(2000);
-      createSpinner.text = 'Updating the package.json file...';
+
+      createSpinner.stopAndPersist({
+        symbol: chalk.green('✔'),
+        text: `Template files copied successfully!`,
+      });
+
+      createSpinner.start('Updating the package.json file');
 
       // Read the package.json file
 
@@ -148,6 +175,17 @@ export default async function createProjectFromTemplate(
 
       // Removing the temporary folder
       rimraf.sync(tmpFolder);
+
+      await git.cwd(projectPath);
+
+      await sleep(2000);
+
+      createSpinner.stopAndPersist({
+        symbol: chalk.green('✔'),
+        text: `Package.json file updated successfully!`,
+      });
+
+      createSpinner.start('Initializing the git repository');
 
       // Initializing the git repository
       await git.init();
