d39be7b3af2082e4630f7946fc572c1c3646e28a
dilane3 <komboudilane125@gmail.com>
2025-03-21 13:31:39 +0100

    chore(docs): update style for pagination component

--- docs/src/components/molecules/pagination.tsx
+++ docs/src/components/molecules/pagination.tsx
@@ -15,14 +15,20 @@ type Props = {
 export default function Pagination({ next, prev }: Props) {
   return (
     <section className="pagination w-full flex items-center justify-center gap-4 mt-10">
-      <Link to={prev.href} className="w-1/2">
+      <Link
+        to={prev.href}
+        className="w-1/2 border-[1px] border-border hover:border-primary transition-all py-4 px-2 rounded-sm"
+      >
         <div className="w-full flex items-center justify-start gap-4">
           <ChevronLeft size={20} className="text-foreground/70" />
           <span>{prev.label}</span>
         </div>
       </Link>
 
-      <Link to={next.href} className="w-1/2">
+      <Link
+        to={next.href}
+        className="w-1/2 border-[1px] border-border hover:border-primary transition-all py-4 px-2 rounded-sm"
+      >
         <div className="w-full flex justify-end items-center gap-4">
           <span>{next.label}</span>
           <ChevronRight size={20} className="text-foreground/70" />
