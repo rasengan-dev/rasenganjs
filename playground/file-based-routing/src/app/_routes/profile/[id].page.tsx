bf7ac7ce975005aa0a3ef46e4229985f69578f05
dilane3 <komboudilane125@gmail.com>
2025-05-28 10:34:58 +0100

    chore: adding profile/[id].page.tsx file into playground

---
+++ playground/file-based-routing/src/app/_routes/profile/[id].page.tsx
@@ -0,0 +1,18 @@
+import { PageComponent } from 'rasengan';
+
+const Page: PageComponent = () => {
+  return (
+    <section className="w-full h-full bg-white flex flex-col items-center justify-center px-[20px] md:px-[50px] xl:px-[200px] font-comfortaa">
+      Profile page
+    </section>
+  );
+};
+
+const metadata = {
+  title: 'Profile',
+  description: 'Profile page',
+};
+
+Page.metadata = metadata;
+
+export default Page;
