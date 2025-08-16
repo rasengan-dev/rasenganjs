e5f69fe252b1aefa879ec63f3782db88d1fe9217
dilane3 <komboudilane125@gmail.com>
2025-08-16 14:45:30 +0100

    chore: update deps

--- playground/rasengan-i18n-demo/.rasengan/deps/react-dom.js
+++
@@ -1,4 +0,0 @@
-import { require_react_dom } from './chunk-ANVNUK3W.js';
-import './chunk-LPOV34HP.js';
-import './chunk-G3PMV62Z.js';
-export default require_react_dom();
--- playground/rasengan-i18n-demo/.rasengan/deps/react_jsx-runtime.js
+++
@@ -1,4 +0,0 @@
-import { require_jsx_runtime } from './chunk-NBOQIQQF.js';
-import './chunk-LPOV34HP.js';
-import './chunk-G3PMV62Z.js';
-export default require_jsx_runtime();
