cfe21dbcbd71c9f23ad9b4e177cef908be6f56b5
dilane3 <komboudilane125@gmail.com>
2026-01-03 16:46:27 +0100

    feat: update templates to match rasengan v1.2.0 requirements

--- packages/create-rasengan/templates/config-based/tailwind-v3-js/src/main.jsx
+++ packages/create-rasengan/templates/config-based/tailwind-v3-js/src/main.jsx
@@ -1,8 +1,7 @@
 import '@rasenganjs/image/css';
 import '@/styles/index.css';
 import React from 'react';
-import AppRouter from '@/app/app.router';
 
 export default function App({ Component, children }) {
-  return <Component router={AppRouter}>{children}</Component>;
+  return <Component>{children}</Component>;
 }
